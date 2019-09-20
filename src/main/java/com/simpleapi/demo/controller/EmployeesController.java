d18d45730d56231b13413b45b64829827ee6372d
JG <eta1405@hotmail.com>
2019-09-20 13:55:54 -0500

    Modified controller annotations for Employees Controller

--- src/main/java/com/simpleapi/demo/controller/EmployeesController.java
+++ src/main/java/com/simpleapi/demo/controller/EmployeesController.java
@@ -5,21 +5,23 @@ import com.simpleapi.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/employees")
 public class EmployeesController {
     @Autowired
     EmployeeService employeeService;
 
-    @GetMapping("/employees/all")
+    @GetMapping("/all")
     public List<Employees> getAll(){
         return employeeService.findAll();
     }
 
-    @GetMapping("/employees/{id}")
+    @GetMapping("/{id}")
         public Employees findById(@PathVariable final Integer id){
             return employeeService.findById(id);
         }
